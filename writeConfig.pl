b25457bea0bc060e6f7cac3c86dc8ef58aebd545
systemsAtDrew <145495296+systemsAtDrew@users.noreply.github.com>
2023-09-19 15:17:34 -0400

    Create writeConfig.pl
    config.pl will be required by any script that needs to use FOLIO's API

---
+++ writeConfig.pl
@@ -0,0 +1,20 @@
+#!/usr/bin/perl
+
+$j = "Content-type: application/json";
+$url = "https://okapi-drew.folio.ebsco.com";
+
+$result = `curl -i -s -X POST -H '$j' -H 'X-Okapi-Tenant: [tenantId]' -d '{"username": "[username]", "password": "[password]"}' $url/authn/login`;
+@parts = split(/\n/,$result);
+foreach $part (@parts) {
+        if ($part =~ /^x-okapi-token:/) {
+                $x = "X-Okapi-Token: " . substr($part,15);
+                $x =~ s/^M//g; $note2self = "*** that looks like ^M but it is not - it is Ctrl-V Ctrl-M ***";
+        }
+}
+
+open(w,">config.pl");
+print w qq[
+\$url = "$url";
+\$headers = "-H '$j' -H '$x'";
+];
+close(w);
